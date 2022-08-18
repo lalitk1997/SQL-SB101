fcacbfacafa781b2749c62882f8de2bd8f5d7c87
Lalit Kumar <lalit.k1997@gmail.com>
2022-08-19 02:36:07 +0530

    Day-5 Assign JDBC Part-1
    Day-5 Assign JDBC Part-1

---
+++ Day-5/Assignment/Assign.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE db1;
+USE db1;
+CREATE TABLE employees(eid INT, name VARCHAR(15), address VARCHAR(15), salary INT,
+PRIMARY KEY(eid));
+SHOW TABLES;
+DESC employees;
---
+++ Day-5/Assignment/Assign/src/com/masai/JdbcConnect.java
@@ -0,0 +1,31 @@
+package com.masai;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JdbcConnect {
+	public static void main(String args[]) {
+		try {
+			// Class class | forName-> static method of Class
+			// ClassNotFoundException thrown (Checked Exception)
+			// location of Driver Class (JDBC JAR)
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} 
+		
+		String url = "jdbc:mysql://localhost:3306/db1";
+		// Connection -> Interface in java.sql Package
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection(url, "root", "root");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if(con != null) {
+			System.out.println("Connected to db ....");
+		}
+	}
+}
